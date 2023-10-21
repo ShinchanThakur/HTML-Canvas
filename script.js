7060286a714aba2c448260d97ca4d62819600f81
ShinchanThakur <rohanthakurarmy@gmail.com>
2023-10-21 09:08:55 +0530

    Fixed rectangle resizing problem

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-ctx.fillStyle = 'white';
-ctx.fillRect(10, 20, 150, 50);
+createRectangle();
+function createRectangle() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.fillStyle = 'white';
+    ctx.fillRect(10, 20, 150, 50);
+}
+
+window.addEventListener('resize', fixRectanglePostionAndSize);
+function fixRectanglePostionAndSize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.fillStyle = 'white';
+    ctx.fillRect(10, 20, 150, 50);
+}
