2f919a7a9ab6feb59a120d10bc8ee01200cd5997
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-10-22 05:50:52 +0530

    Created Particle class

---
+++ particleSystem.js
@@ -0,0 +1,36 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+fixCanvasPositionAndSize();
+function fixCanvasPositionAndSize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+window.addEventListener('resize', fixCanvasPositionAndSize);
+
+const mouse = {
+    x: undefined,
+    y: undefined
+}
+
+function createCircle() {
+    ctx.fillStyle = 'white';
+    ctx.beginPath();
+    ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
+    ctx.fill();
+}
+
+canvas.addEventListener('mousemove', setXYAtMousePosition);
+function setXYAtMousePosition(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+}
+
+class Particle {
+    constructor() {
+        this.x = mouse.x;
+        this.y = mouse.y;
+        this.size = Math.random * 5 + 1; // 1 to 6
+        this.speedX = Math.random * 3 - 1.5; // -1.5 to 1.5
+        this.speedY = Math.random * 3 - 1.5;
+    }
+}
