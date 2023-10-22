83da3424626bc0ce1c0969042b913113daba6676
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-10-22 06:17:45 +0530

    Gave particles random sizes and reducing their size constantly

--- particleSystem.js
+++ particleSystem.js
@@ -11,18 +11,19 @@ class Particle {
     constructor() {
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
-        this.size = Math.random() * 5 + 1; // 1 to 6
+        this.size = Math.random() * 15 + 1; // 1 to 16
         this.speedX = Math.random() * 3 - 1.5; // -1.5 to 1.5
         this.speedY = Math.random() * 3 - 1.5;
     }
     update() {
         this.x += this.speedX;
         this.y += this.speedY;
+        if (this.size > 0.2) this.size -= 0.1;
     }
     createCircle() {
         ctx.fillStyle = 'white';
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.fill();
     }
 }
