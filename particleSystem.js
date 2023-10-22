3cadbc1c4a703fd7f8684e321e0f07e8cdd887de
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-10-22 06:11:24 +0530

    Created Particles effect on screen

--- particleSystem.js
+++ particleSystem.js
@@ -7,24 +7,13 @@ function fixCanvasPositionAndSize() {
 }
 window.addEventListener('resize', fixCanvasPositionAndSize);
 
-const mouse = {
-    x: undefined,
-    y: undefined
-}
-
-canvas.addEventListener('mousemove', setXYAtMousePosition);
-function setXYAtMousePosition(event) {
-    mouse.x = event.x;
-    mouse.y = event.y;
-}
-
 class Particle {
     constructor() {
-        this.x = mouse.x;
-        this.y = mouse.y;
-        this.size = Math.random * 5 + 1; // 1 to 6
-        this.speedX = Math.random * 3 - 1.5; // -1.5 to 1.5
-        this.speedY = Math.random * 3 - 1.5;
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 5 + 1; // 1 to 6
+        this.speedX = Math.random() * 3 - 1.5; // -1.5 to 1.5
+        this.speedY = Math.random() * 3 - 1.5;
     }
     update() {
         this.x += this.speedX;
@@ -33,7 +22,28 @@ class Particle {
     createCircle() {
         ctx.fillStyle = 'white';
         ctx.beginPath();
-        ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
+        ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
         ctx.fill();
     }
 }
+
+const particlesArray = [];
+function create100Particles() {
+    for (let i = 0; i < 100; i++)
+        particlesArray.push(new Particle());
+}
+create100Particles();
+
+function updateAndDrawParticles() {
+    particlesArray.forEach((particle) => {
+        particle.update();
+        particle.createCircle();
+    });
+}
+
+function createParticleEffectOnScreen() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    updateAndDrawParticles();
+    requestAnimationFrame(createParticleEffectOnScreen);
+}
+createParticleEffectOnScreen();
